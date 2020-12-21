7b0a98bd259069ed5a55ffde90c4254b4d0b9012
Artem Syritsa <80500451040j@gmail.com>
2020-12-21 11:49:22 +0200

    Initial Commit

---
+++ NativeFramework/NativeFramework.h
@@ -0,0 +1,18 @@
+//
+//  NativeFramework.h
+//  NativeFramework
+//
+//  Created by Artem Syritsa on 21.12.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NativeFramework.
+FOUNDATION_EXPORT double NativeFrameworkVersionNumber;
+
+//! Project version string for NativeFramework.
+FOUNDATION_EXPORT const unsigned char NativeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NativeFramework/PublicHeader.h>
+
+
